1187c5e198450bd38b583e6da1929c7ba00fc87e
Andrey Nikonov <a.nikonov@samsung.com>
2014-03-05 18:00:20 +0400

    add webviewutils

---
+++ androidutils/src/main/java/com/appmobileos/android/utils/WebViewUtils.java
@@ -0,0 +1,43 @@
+package com.samsung.utils;
+
+import android.util.Log;
+
+/**
+ * Created by a.nikonov on 29.10.13.
+ */
+public class WebViewUtils {
+    private static final String TAG = "WebViewUtils";
+    /**
+     * This is function helper which help create correct string for execute in WebView
+     *
+     * @param javaScriptFunctionName this is name function which have in JavaScript code
+     * @param sendJsonFile           if true first argument must be JSON. Support send only one JSON file and many string arguments.
+     * @param argumentFunction       there are arguments which will be add in JavaScript function.
+     * @return string type which can be execute in WebView
+     */
+    public static String createJsExecuteString(String javaScriptFunctionName, boolean sendJsonFile, String... argumentFunction) {
+        String mJsRequest = "javascript:" + javaScriptFunctionName + (sendJsonFile ? "('" : "(\"");
+        StringBuilder mArguments = new StringBuilder();
+        String mFinalRequest;
+        if (argumentFunction!=null && argumentFunction.length > 0) {
+            for (int i = 0; i < argumentFunction.length; i++) {
+                if (i == argumentFunction.length - 1) {
+                    String endBlock = "\")";
+                    if (sendJsonFile) {
+                        if (sendJsonFile && !(argumentFunction.length > 1)) {
+                            endBlock = "')";
+                        }
+                    }
+                    mArguments.append(argumentFunction[i]).append(endBlock);
+                } else {
+                    mArguments.append(argumentFunction[i]).append((sendJsonFile && i == 0 ? "',\"" : "\",\""));
+                }
+            }
+            mFinalRequest = mJsRequest + mArguments.toString();
+        } else {
+            mFinalRequest = mJsRequest + (sendJsonFile ? "')" : "\")");
+        }
+        Log.i(TAG, " What create createJsExecuteString = " + mFinalRequest);
+        return mFinalRequest;
+    }
+}
---
+++ androidutils/src/main/java/com/appmobileos/android/utils/color/Colors.java
@@ -0,0 +1,29 @@
+package com.appmobileos.android.utils.color;
+
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by anikonov on 12/5/13.
+ */
+public class Colors {
+
+    public static int randomColor() {
+        Random rnd = new Random();
+        return Color.argb(255, randInt(rnd, 60, 256), randInt(rnd, 60, 256), randInt(rnd, 60, 256));
+    }
+
+    public static int randInt(Random rand, int min, int max) {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+    public static int colorGray() {
+        int rgbNum = 255 - (int) ((15/50.0)*255.0);
+        return Color.argb(255, rgbNum, rgbNum, rgbNum);
+    }
+}
